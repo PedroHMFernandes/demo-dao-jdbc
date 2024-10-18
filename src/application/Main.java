ed85718bf43108b01323051ce815d4dc7817d214
Pedro Fernandes <pedrovosvisk10@gmail.com>
2024-10-17 21:52:30 -0300

    Seller entity class

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,11 +1,17 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
 
         Department dpt = new Department(1, "Books");
+        Seller seller = new Seller(1, "John Doe", "johndoe@gmail.com", new Date(), 3000.0, dpt);
+
         System.out.println(dpt);
+        System.out.println(seller);
     }
 }
